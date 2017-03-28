71d317cfb54ebd9e6cafa26e567ddacf8c9c91c7
Michael Smith <15mikesmith@gmail.com>
2017-03-28 03:41:51 -0400

    add

---
+++ CookingBuddy2/app/src/main/java/com/schwee/cookingbuddy/EasyActivity2.java
@@ -0,0 +1,13 @@
+package com.schwee.cookingbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EasyActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userrecipe);
+    }
+}
